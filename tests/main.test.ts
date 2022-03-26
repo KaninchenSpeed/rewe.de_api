c765bb7ef4f36adee1d14d495639171e48ffc7e3
KaninchenSpeed <fhs222@outlook.de>
2022-03-26 14:22:06 +0100

    added tests for get markets

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,13 @@
 import rewe from '../src/main'
 
+rewe.getDelivery(67346).then(res => { //obviously random
+    console.log(res)
+})
+
+rewe.getMarkets(67346, 'pickup').then(res => {
+    console.log(res[0])
+})
+
 rewe.getProducts(840838).then(res => {
     console.log(res)
 })
